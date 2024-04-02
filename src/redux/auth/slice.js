d38a630e27f8be6494a241df899df0eae77c5ddb
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-03 00:58:18 +0200

    create-redux-auth

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { login, logout, refreshUser, register } from './operations';
+
+const slice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: {
+      name: null,
+      email: null,
+    },
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoggedIn = true;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoggedIn = true;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(logout.fulfilled, state => {
+        state.isLoggedIn = false;
+        state.user = {
+          name: null,
+          email: null,
+        };
+        state.token = null;
+      })
+      .addCase(refreshUser.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      });
+  },
+});
+
+export default slice.reducer;
