1cc948f07832598d1d40fe6265ddd418b7f1ae4f
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-03 00:56:55 +0200

    create-searchBox

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,30 @@
+import css from './SearchBox.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter } from '../../redux/filters/slice';
+import { selectFilter } from '../../redux/filters/selectors';
+import { TextField } from '@mui/material';
+
+const SearchBox = () => {
+  const value = useSelector(selectFilter);
+  const dispatch = useDispatch();
+
+  const handleChange = e => {
+    dispatch(changeFilter(e.target.value));
+  };
+
+  return (
+    <div className={css.container}>
+      <TextField
+        id="standard-basic"
+        label="Search"
+        variant="standard"
+        size="normal"
+        value={value}
+        onChange={handleChange}
+        fullWidth
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
