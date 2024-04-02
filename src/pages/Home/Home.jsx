660a88fcda792dce0ebea5346dd3d778b421f818
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-03 00:57:35 +0200

    create-home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+// import css from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Welcome to Home Page</p>
+      <p>
+        Organize your contacts like never before. Log in to use all the features.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
