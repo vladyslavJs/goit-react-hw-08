5de5a274dea626e45a08db849127f5ef282c59a3
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-03 23:58:47 +0200

    update-appBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,14 +3,14 @@ import { selectIsLoggedIn, selectIsRefreshing } from '../../redux/auth/selectors
 import AuthNav from '../AuthNav/AuthNav';
 import Navigation from '../Navigation/Navigation';
 import UserMenu from '../UserMenu/UserMenu';
-import c from './AppBar.module.css';
+import css from './AppBar.module.css';
 
 const AppBar = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
 
   return (
-    <header className={c.header}>
+    <header className={css.container}>
       <Navigation />
       {!isRefreshing && <div>{isLoggedIn ? <UserMenu /> : <AuthNav />}</div>}
     </header>
