a7a6355e630adc13a1e97cd8e40e7734cd5c27ba
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-03 23:59:51 +0200

    update-loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,16 @@
-import { ThreeDots } from 'react-loader-spinner';
+import { DNA } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <ThreeDots
+      <DNA
         visible={true}
-        height="40"
-        width="40"
-        color="black"
-        radius="9"
-        ariaLabel="three-dots-loading"
+        height="80"
+        width="80"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
       />
     </div>
   );
